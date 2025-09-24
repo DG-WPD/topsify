baa7ca7750836aa30d14c557475e2eb427f2122a
DG-WPD <dlgreen@nationalgrid.co.uk>
2025-09-24 12:24:25 +0000

    commit

--- exercises/5a.sql
+++ exercises/5a.sql
@@ -1,1 +1,4 @@
 -- How many tracks are longer than 5 minutes?
+SELECT count (id)
+FROM tracks
+WHERE duration_ms >= 5*60*1000;
--- exercises/5b.sql
+++ exercises/5b.sql
@@ -1,3 +1,5 @@
 -- Find the average valence of songs in the database
 -- https://community.spotify.com/t5/Spotify-for-Developers/Valence-as-a-measure-of-happiness/td-p/4385221
 -- (Hint: use the features table, no need to JOIN)
+SELECT AVG(valence) 
+FROM features; -- Result was 0.445157159353349 yet teh answer required is 0.445157159353348?
