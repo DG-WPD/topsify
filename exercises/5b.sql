3663c169d337ea3ce98271f7972c3c45570fe83b
DG-WPD <dlgreen@nationalgrid.co.uk>
2025-09-24 14:44:13 +0000

    commit

--- exercises/5b.sql
+++ exercises/5b.sql
@@ -2,4 +2,4 @@
 -- https://community.spotify.com/t5/Spotify-for-Developers/Valence-as-a-measure-of-happiness/td-p/4385221
 -- (Hint: use the features table, no need to JOIN)
 SELECT AVG(valence) 
-FROM features; -- Result was 0.445157159353349 yet teh answer required is 0.445157159353348?
+FROM features; -- Result was 0.445157159353349 yet the answer required is 0.445157159353348?
