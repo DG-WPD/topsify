8deafca3b2deb7909d0093452c14a49e066c30b1
DG-WPD <dlgreen@nationalgrid.co.uk>
2025-09-24 12:59:53 +0000

    commit

--- exercises/6a.sql
+++ exercises/6a.sql
@@ -2,3 +2,8 @@
 -- Include a count of how many albums they have released
 -- Order your results by artist name in ascending order
 -- (You should group the artists by id, not by name)
+SELECT a.name, count(al.name)
+FROM artists a, albums al
+WHERE a.id = al.artist_id
+GROUP BY a.name
+ORDER BY a.name
--- exercises/6b.sql
+++ exercises/6b.sql
@@ -1,3 +1,8 @@
 -- Create a list of all album names
 -- Include the total duration of the album
 -- Sort the results by album duration, with the longest first
+SELECT a.name, sum(duration_ms) as duration
+FROM albums a, tracks t
+WHERE a.id = t.album_id
+GROUP BY a.id
+ORDER BY duration DESC
