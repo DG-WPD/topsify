5d0ff247a525c8389f8f8b766aa5d8e2a3f76d48
DG-WPD <dlgreen@nationalgrid.co.uk>
2025-09-24 11:56:51 +0000

    commit

--- exercises/4a.sql
+++ exercises/4a.sql
@@ -1,3 +1,6 @@
 -- List the name of every album in the database
 -- Include the artist name
 -- (Make sure to select the album name first, then the artist name)
+SELECT al.name, ar.name
+FROM albums al, artists ar
+WHERE al.artist_id = ar.id;
--- exercises/4b.sql
+++ exercises/4b.sql
@@ -1,3 +1,8 @@
 -- List the 10 slowest tracks in the database
 -- Order by tempo, with the slowest first
 -- Include the track id, track title and track tempo
+SELECT t.id, t.name, f.tempo
+FROM tracks t, features f
+WHERE t.id = f.track_id
+ORDER BY f.tempo
+LIMIT 10;
